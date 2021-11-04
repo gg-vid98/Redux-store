e37f2ff1b24e130c5c82e7fecf605342f98f4ac4
Vidya Arunachalam <vidya.arunachalam@publicissapient.com>
2021-11-04 13:40:23 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import './App.css';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail'
+function App() {
+  return (
+    <div>
+      <Router>
+        
+     <Header/>
+     <Switch>
+     <Route path="/" exact component={ProductListing}/>
+     <Route path="/product/:productId" exact component={ProductDetail}/>
+     <Route>404</Route>
+     </Switch>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
